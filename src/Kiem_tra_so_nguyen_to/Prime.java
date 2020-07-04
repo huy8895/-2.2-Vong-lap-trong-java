0258142d7813401ae84bfc21170eee955fae6dc0
huy8895 <huy8895@gmail.com>
2020-07-04 14:32:02 +0700

    thuc hanh kiem tra so nguyen to

--- src/Kiem_tra_so_nguyen_to/Prime.java
+++ src/Kiem_tra_so_nguyen_to/Prime.java
@@ -1,26 +1,27 @@
-import javax.swing.*;
+package Kiem_tra_so_nguyen_to;
+
 import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("nhap vao mot so :");
+        System.out.println("Enter a number:" );
         int number = scanner.nextInt();
+        boolean isPrime = true;
 
-        if (number < 2 ){
-            System.out.println(number + " is not a prime");
+        if (number < 2){
+            System.out.println(number + " is NOT a prime");
         } else {
             int i = 2;
-            boolean check = true;
-            while ( i <= Math.sqrt(number)){
+            while (i <= Math.sqrt(number)){
                 if (number % i == 0){
-                    check = false;
+                    isPrime = false ;
                     break;
                 }
-                i ++;
-                }
-            if (check){
+                i++;
+            }
+            if (isPrime){
                 System.out.println(number + " is a prime");
             } else {
                 System.out.println(number + " is NOT a prime");
