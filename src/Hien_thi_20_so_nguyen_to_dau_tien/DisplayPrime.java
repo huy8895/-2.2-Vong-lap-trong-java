a6dcbee7e0aa0e6d160dae38b9c2dd58b3598367
huy8895 <huy8895@gmail.com>
2020-07-04 16:29:48 +0700

    bai tap hien thi 20 so nguyen to dau tien

--- src/Hien_thi_20_so_nguyen_to_dau_tien/DisplayPrime.java
+++ src/Hien_thi_20_so_nguyen_to_dau_tien/DisplayPrime.java
@@ -1,4 +1,41 @@
 package Hien_thi_20_so_nguyen_to_dau_tien;
 
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
+import java.util.Scanner;
+
 public class DisplayPrime {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("nhap so luong so nguyen to can hien: ");
+        int number = scan.nextInt();
+        showPrime(number);
+    }
+
+    public static boolean isPrime (int number){
+        boolean result = true;
+        if (number < 2){
+            result = false;
+        }
+        for (int i = 2; i <= Math.sqrt(number);i ++){
+            if (number % i == 0){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void showPrime(int number){
+        int count = 0;
+        int i = 0;
+        while(count < number){
+            if (isPrime(i)){
+                System.out.print(i + " ");
+                count ++;
+            }
+            i++;
+        }
+    }
+
 }
