aa9845fe77541fbd7095b10eb7e5313b55ee673e
huy8895 <huy8895@gmail.com>
2020-07-04 15:10:04 +0700

    thuc hanh tim uoc chung lon nhat

---
+++ src/Tim_uoc_chung_lon_nhat/Divisor.java
@@ -0,0 +1,28 @@
+package Tim_uoc_chung_lon_nhat;
+
+import java.util.Scanner;
+
+public class Divisor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a = input.nextInt();
+        System.out.println("Enter b: ");
+        int b = input.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0){
+            System.out.println("Not greatest common factor");
+        }
+        while (a != b){
+            if (a > b){
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
